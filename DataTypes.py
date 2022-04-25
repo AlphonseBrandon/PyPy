59780eb30a4bc24712b9238dcd5f122a839048ef
AlphonseBrandon <alphonse.brand@gmail.com>
2022-04-25 21:34:46 +0100

    Data structures

--- DataTypes.py
+++ DataTypes.py
@@ -4,15 +4,29 @@
 
 fruits = ["apple", "Banana", "orange"]
 
+fruits.pop(1)
+
+food = ("rice", "beans", "wheat", "corn")
+
+animal = {"mammal": "Lion", "carnivores": "Tiger"}
+
+print(animal["mammal"])
+
+# print(animal)
+
+
+# print(food)
+
+# add_fruits = ["Guava", "Peer"]
+
+# fruits.append(add_fruits)
+
+# fruits.append("Peer")
+
+# fruits.append("guava")
+
 # print(fruits)
 
-# List indexing
-
-print(fruits[1])
-
-# Range
-
-print(fruits[:1])
 
 
 
